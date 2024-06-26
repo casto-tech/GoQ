b57b1bf5e80a4d867d40a411db4838ce0ae900e5
casto-tech <castojr@yahoo.com>
2024-06-26 16:50:16 -0700

    flake8 fix

--- tests/test_config_loads.py
+++ tests/test_config_loads.py
@@ -4,7 +4,7 @@ from app import app
 
 def test_config_loads_from_env(test_client):
     assert os.getenv("SERVER") == app.config['MAIL_SERVER']
-    assert os.getenv("PORT") == app.config['MAIL_PORT'] 
+    assert os.getenv("PORT") == app.config['MAIL_PORT']
     assert os.getenv("USERNAME") == app.config['MAIL_USERNAME']
     assert os.getenv("PASSWORD") == app.config['MAIL_PASSWORD']
     assert os.getenv("TLS") == app.config['MAIL_USE_TLS']
