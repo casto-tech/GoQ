2c942cf38a100ca09022a376757545ad38fc4b11
casto-tech <castojr@yahoo.com>
2024-06-26 15:35:37 -0700

    unit-test test failure

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GoQ
+[![Unit-Test](https://github.com/casto-tech/GoQ/actions/workflows/unit-test.yml/badge.svg?event=push)](https://github.com/casto-tech/GoQ/actions/workflows/unit-test.yml)
--- tests/test_home_route.py
+++ tests/test_home_route.py
@@ -5,6 +5,6 @@ from app import app
 
 def test_home_route(test_client):
     response = app.test_client().get('/')
-    assert response.status_code == 200
+    assert response.status_code == 100
     assert response.status_code != 404
 
