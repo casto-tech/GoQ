dc77899785a552792614d2124cdaeabf35697d84
casto-tech <castojr@yahoo.com>
2024-08-29 17:13:34 -0700

    fixed failing config test

--- tests/test_config_loads.py
+++ tests/test_config_loads.py
@@ -3,9 +3,8 @@ from application import application
 
 
 def test_config_loads_from_env(test_client):
-    assert os.getenv("SERVER") == application.config['MAIL_SERVER']
-    assert os.getenv("PORT") == application.config['MAIL_PORT']
-    assert os.getenv("USERNAME") == application.config['MAIL_USERNAME']
-    assert os.getenv("PASSWORD") == application.config['MAIL_PASSWORD']
-    assert os.getenv("TLS") == application.config['MAIL_USE_TLS']
-    assert os.getenv("SSL") == application.config['MAIL_USE_SSL']
+    assert os.getenv("SERVICE_ACCOUNT_FILE") == 'sa-key.json'
+    assert os.getenv("USER_TO_IMPERSONATE") == 'info@greensonq.com'
+    assert os.getenv("SCOPES") == 'https://www.googleapis.com/auth/gmail.send'
+    assert os.getenv("TO") == 'joey@greensonq.com'
+    assert os.getenv("SENDER") == 'info@greensonq.com'
