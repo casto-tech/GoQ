7b265e0ebd497b25541b5eb0a10a057483f404f3
casto-tech <castojr@yahoo.com>
2025-03-13 16:13:20 -0700

    add 404 test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,3 @@
-# import pytest  # type: ignore
-# from application import application
-
-
-# @pytest.fixture
-# def test_client():
-#     application.config['TESTING'] = True
-#     application.config['MAIL_DEBUG'] = True
-#     application.config['MAIL_SUPPRESS_SEND'] = True
-#     with application.test_client() as client:
-#         yield client
-
 import pytest  # type: ignore
 from application import application
 from dotenv import load_dotenv
