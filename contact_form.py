bfc921ba913ec2a1a98bab89fbd12f2477646fd9
casto-tech <castojr@yahoo.com>
2025-03-13 17:12:58 -0700

    added character limit to 50 to contact form

--- contact_form.py
+++ contact_form.py
@@ -1,6 +1,25 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, TextAreaField
+# from wtforms.validators import DataRequired, Email, Regexp, ValidationError, Length
+
+
+# def validate_name(form, field):
+#     """Custom validator to block specific characters in the name field."""
+#     blocked_chars = r"%/\<>:${\}_&|"
+#     for char in blocked_chars:
+#         if char in field.data:
+#             raise ValidationError(f"Name cannot contain the following characters: {blocked_chars}")
+
+
+# class ContactForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired(), validate_name, Length(max=50)])
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\+?\d{7,15}$', message="Invalid phone number")])
+#     message = TextAreaField('Message', validators=[DataRequired()])
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired, Email, Regexp, ValidationError
+from wtforms.validators import DataRequired, Email, Regexp, ValidationError, Length
 
 
 def validate_name(form, field):
@@ -12,7 +31,7 @@ def validate_name(form, field):
 
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired(), validate_name])
+    name = StringField('Name', validators=[DataRequired(), validate_name, Length(max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\+?\d{7,15}$', message="Invalid phone number")])
     message = TextAreaField('Message', validators=[DataRequired()])
