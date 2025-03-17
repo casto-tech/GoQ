a02f7839b599b7a212ad9f9833eaa52b6784cfca
casto-tech <castojr@yahoo.com>
2025-03-17 11:44:39 -0700

    update phone regex & error text

--- contact_form.py
+++ contact_form.py
@@ -5,22 +5,22 @@ from wtforms.validators import DataRequired, Email, Regexp, ValidationError, Len
 
 def validate_name(form, field):
     """Custom validator to block specific characters in the name field."""
-    blocked_chars = r"%/\<>:${\}_&|"
+    blocked_chars = r"%/\<>:${}_&|#@;*()^"
     for char in blocked_chars:
         if char in field.data:
-            raise ValidationError(f"Name cannot contain the following characters: {blocked_chars}")
+            raise ValidationError("Name cannot contain special characters.")
 
 
 def validate_message(form, field):
     """Custom validator to block specific characters in the message field."""
-    blocked_chars = r"/\\<>:{}_|"
+    blocked_chars = r"/\<>:{}_|#@;*()^"
     for char in blocked_chars:
         if char in field.data:
-            raise ValidationError(f"Message cannot contain the following characters: {blocked_chars}")
+            raise ValidationError("Message cannot contain special characters.")
 
 
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), validate_name, Length(max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\+?\d{7,15}$', message="Invalid phone number")])
+    phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\d{10}$|^\d{3}-\d{3}-\d{4}$|^\d{7}$|^\d{3}-\d{4}$', message="Invalid phone number: Do not inlcude ( ).")])
     message = TextAreaField('Message', validators=[DataRequired(), validate_message])
